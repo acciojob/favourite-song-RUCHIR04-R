921a20776d5baa6bd1f060f2210c2dcf1445302c
RUCHIR04-R <ruchirgawture@gmail.com>
2025-01-28 17:17:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your code here
+document.addEventListener('DOMContentLoaded', function () {
+    var link = document.querySelector('a');
+    link.addEventListener('click', function () {
+        alert('Enjoy listening to Shape of You!');
+    });
+});
